7b197e9be9dc4ca48b5ca9aa008f8fd10063cab4
PinguITC <tchu326@lasierra.edu>
2024-03-16 15:39:31 -0700

    Started menue branch try merging for testing purposes

---
+++ EaglesNestSite/Views/Home/Menue.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EaglesNestSite.Views.Home
+{
+    public class MenueModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

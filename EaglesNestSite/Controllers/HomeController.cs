7ddce58910d3c5a03bd37812ea8cc3c24fe8829f
PinguITC <tchu326@lasierra.edu>
2024-03-16 20:01:17 -0700

    Menue push attempt 10000

--- EaglesNestSite/Controllers/HomeController.cs
+++ EaglesNestSite/Controllers/HomeController.cs
@@ -29,6 +29,12 @@ public class HomeController : Controller
     }
 
 
+    public IActionResult Menue()
+    {
+        return View();
+    }
+
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {

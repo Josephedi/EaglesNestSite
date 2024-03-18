6582758e38c96944f504e0626a78438fda24705e
Josephedi <jdia288@lasierra.edu>
2024-03-18 01:00:58 -0700

    updated the nav bar

--- EaglesNestSite/Controllers/HomeController.cs
+++ EaglesNestSite/Controllers/HomeController.cs
@@ -35,6 +35,12 @@ public class HomeController : Controller
     }
 
 
+    public IActionResult AboutMe()
+    {
+        return View();
+    }
+
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
